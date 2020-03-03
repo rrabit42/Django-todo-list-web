97587b44c53d548623bb782a4e27bcf8be5a0d5a
rrabit42 <gegiraffe@naver.com>
2020-03-03 21:45:44 +0900

    [ADD] todo_board views

--- todoSubject/todo_board/views.py
+++ todoSubject/todo_board/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views import View, generic
+from todo_board.models import TodoList
 
 
 class Todo_main(generic.TemplateView):
     def get(self, request, *args, **kwargs):
-        template_name = 'todo_main/index.html'
-        return render(request, template_name)
+        template_name = 'todo_board/todo_list.html'
+        todo_list = TodoList.objects.all()
+        return render(request, template_name, {"todo_list" : todo_list})
 
