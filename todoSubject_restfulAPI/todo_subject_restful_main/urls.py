c4a74795077a3034eff4bf2d6ba26d9a47561ad8
rrabit42 <gegiraffe@naver.com>
2020-03-07 18:40:25 +0900

    [ADD] RestAPI - Update, Delete

--- todoSubject_restfulAPI/todo_subject_restful_main/urls.py
+++ todoSubject_restfulAPI/todo_subject_restful_main/urls.py
@@ -10,7 +10,10 @@ app_name = 'todo_subject_restful_main'
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', views.Todo_subject_restful_main.as_view(), name='todo_list'),
+    # Primary key를 이용하여 데이터에 각각 접근
     path('todo_list/<int:no>', views.Todo_subject_restful_detail.as_view(), name='todo_detail'),
+    path('todo_list/<int:no>/update', views.Todo_subject_restful_update.as_view(), name='todo_update'),
+    path('todo_list/<int:no>/delete', views.Todo_subject_restful_delete.as_view(), name='todo_delete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todoSubject_restfulAPI/todo_subject_restful_main/views.py
+++ todoSubject_restfulAPI/todo_subject_restful_main/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from .models import TodoList
 from .serializers import TodoSerializer, TodoDetailSerializer
 
 
-# list view
+# Read- list view
 class Todo_subject_restful_main(ListAPIView):
     queryset = TodoList.objects.all()
     serializer_class = TodoSerializer
 
 
-# detail view
+# Read - detail view
 class Todo_subject_restful_detail(RetrieveAPIView):
     lookup_field = 'no'
     queryset = TodoList.objects.all()
     serializer_class = TodoDetailSerializer
+
+
+# Update
+class Todo_subject_restful_update(UpdateAPIView):
+    lookup_field = 'no'
+    queryset = TodoList.objects.all()
+    serializer_class = TodoSerializer
+
+
+# Delete
+class Todo_subject_restful_delete(DestroyAPIView):
+    lookup_field = 'no'
+    queryset = TodoList.objects.all()
+    serializer_class = TodoSerializer
+'''
+requests를 이용하면 기존에 db와 연동하는 것이 아닌,
+rest api와 연동하여 프로그램을 작성할 수 있다.
+d = {
+    "title" : "파이썬에서 보냅니다",
+    "content" : "파이썬!"
+}
+딕셔너리 데이터를 넣어주면 됨
+data = requests.put('http://localhost:8088/todo_list/5/update/', data=d)
+print(data)
+'''
