842a3e4834002707ef7075e5cd56613f84ba1abb
rrabit42 <gegiraffe@naver.com>
2020-03-13 01:59:13 +0900

    [ADD] Paginator

--- django_rest_test/post_service/views.py
+++ django_rest_test/post_service/views.py
@@ -1,12 +1,24 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from post_service.models import Post
+
 
 def post_list(request):
-    template = get_template('post_service/post_list.html')
-    context = {
-        'python_world' : 'Python'
-    }
+    page_data = Paginator(Post.objects.all().order_by('title'), 5)
+    page = request.GET.get('page') # GET parameter로 page가 온다고 가정하고 데이터들을 건들임
+    try:
+        posts = page_data.page(page)
+    except PageNotAnInteger:
+        posts = page_data.page(1)
+    except EmptyPage:
+        posts = page_data.page(page_data.num_pages)
 
-    return HttpResponse(template.render(context))
+    context = {
+        'post_list': posts,
+        'current_page': int(page),
+        'total_page': range(1, page_data.num_pages + 1)
+    }
+    return render(request, 'post_service/post_list.html', context)
